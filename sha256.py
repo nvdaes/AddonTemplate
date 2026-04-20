91995bd24a9448bc95ae2bf6b33c0427aa2968be
Noelia Ruiz Martínez <nrm1977@gmail.com>
2026-04-20 19:13:08 +0200

    Try to fix pre-commit configuration

--- sha256.py
+++ sha256.py
@@ -17,17 +17,16 @@ def sha256_checksum(binaryReadModeFiles: list[typing.BinaryIO], blockSize: int =
 	:return: The Sha256 hex digest.
 	"""
 	sha256 = hashlib.sha256()
-	for f in binaryReadModeFiles:
-		with open(f, "rb") as file:
-			assert file.readable() and file.mode == "rb"
-			for block in iter(lambda: file.read(blockSize), b""):
-				sha256.update(block)
+	for file in binaryReadModeFiles:
+		assert file.readable() and file.mode == "rb"
+		for block in iter(lambda: file.read(blockSize), b""):
+			sha256.update(block)
 	return sha256.hexdigest()
 
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument(
+	_ = parser.add_argument(
 		type=argparse.FileType("rb"),
 		dest="file",
 		help="The NVDA addon (*.nvda-addon) to use when computing the sha256.",
